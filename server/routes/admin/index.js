77c5dbcb9537e0389afdd7ea23d0f772328135c8
myhub-ljc <2321170474@qq.com>
2020-02-28 18:10:53 +0800

    分类的增删改查完成

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -8,5 +8,27 @@ module.exports = app => {
     res.send(model)
   })
 
+  router.put('/categories/:id', async(req, res) => {
+    const model = await Categories.findByIdAndUpdate(req.params.id, req.body)
+    res.send(model)
+  })
+
+  router.delete('/categories/:id', async(req, res) => {
+   await Categories.findByIdAndDelete(req.params.id, req.body)
+    res.send({
+      success: true
+    })
+  })
+
+  router.get('/categories', async(req, res) => {
+    const items = await Categories.find().limit(10)
+    res.send(items)
+  })
+
+  router.get('/categories/:id', async(req, res) => {
+    const model = await Categories.findById(req.params.id)
+    res.send(model)
+  })
+
   app.use('/admin/api', router)
 }
